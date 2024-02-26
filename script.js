98239b15222901b6298d71035056ef1a1ccb2ce6
Talika Bajaj <bajajtalika2001@gmail.com>
2024-02-26 18:46:51 +0530

    Add/remove class from quizScreen and endScreen

--- script.js
+++ script.js
@@ -7,7 +7,13 @@ async function getQuestions() {
     let question = await fetch("https://opentdb.com/api.php?amount=1");
     let data = await question.json();
     console.log(data);
-    showQuestion(data.results[0]);
+    console.log(data.response_code);
+    if (data.response_code === 0) {
+        showQuestion(data.results[0]);
+    } else {
+        document.getElementById("question").innerHTML = "Error fetching question.Please try again";
+        score-= 10;
+    }
 
 }
 
@@ -15,33 +21,39 @@ getQuestions();
 
 
 function showQuestion(data) {
-    console.log(data);
-    console.log(data.correct_answer);
+    try {
 
-    question.innerHTML = data.question;
-    const { correct_answer, incorrect_answers } = data;
-    const answer = [correct_answer, ...incorrect_answers];
-    console.log(answer);
+        console.log(data);
+        console.log(data.correct_answer);
 
-    answer.sort(() => Math.random() - 0.5);
-    console.log(answer);
+        question.innerHTML = data.question;
+        const { correct_answer, incorrect_answers } = data;
+        const answer = [correct_answer, ...incorrect_answers];
+        console.log(answer);
 
-    // let answers = document.querySelectorAll(".answer");
-    // for (let i = 0; i< answer.length; i++) {
-    //     answers[i].innerHTML = answer[i];
-    // }
-    let answersList = document.querySelector(".answersList")
-    answersList.innerHTML = `
+        answer.sort(() => Math.random() - 0.5);
+        console.log(answer);
+
+        // let answers = document.querySelectorAll(".answer");
+        // for (let i = 0; i< answer.length; i++) {
+        //     answers[i].innerHTML = answer[i];
+        // }
+        let answersList = document.querySelector(".answersList")
+        answersList.innerHTML = `
     ${answer.map((ans) =>
-        `<button class="btn">${ans}</button>`
-    ).join(" ")} `;
-    // let answersList = document.querySelector(".answersList")
-    answersList.querySelectorAll("button").forEach(element => {
-        if (element.innerHTML === correct_answer) {
-            element.addEventListener("click", correctAnswer)
-        }
+            `<button class="btn">${ans}</button>`
+        ).join(" ")} `;
+        // let answersList = document.querySelector(".answersList")
+        answersList.querySelectorAll("button").forEach(element => {
+            if (element.innerHTML === correct_answer) {
+                element.addEventListener("click", correctAnswer)
+            }
 
-    });
+        });
+    } catch (error) {
+        question.innerHTML = "Error in fetching question. Please Wait";
+        console.log(error);
+    }
 }
 
 function countDown(seconds) {
@@ -71,20 +83,20 @@ function updateScore() {
 }
 
 function endQuiz() {
-    document.getElementById("quizScreen").classList.add("hidden");
+    document.getElementById("quizScreen").classList.remove("active");
     document.getElementById("endScreen").classList.remove("hidden");
     let endMsg = document.getElementById("endMessage");
     let finalScore = document.getElementById("finalScore");
 
     if (score <= 20) {
-        endMsg.innerText = "You played poor";
+        endMsg.innerHTML = `<span style = "color: black">Remarks: </span> You played poor`;
     } else if (score >= 20 && score <= 50) {
-        endMsg.innerText = "You played good";
+        endMsg.innerHTML = `<span style = "color: black">Remarks: </span> You played good`;
     } else {
-        endMsg.innerText = "You played good";
+        endMsg.innerHTML = `<span style = "color: black">Remarks: </span> You played good`;
     }
 
-    finalScore.innerHTML = score;
+    finalScore.innerHTML = `<span style = "font-weight:600">Final score: </span> ${score}`;
 }
 
 
@@ -92,13 +104,14 @@ startQuiz.addEventListener("click", () => {
     countDown(60);
     document.getElementById("start-rules").style.display = "none";
     document.getElementById("startScreen").style.display = "none";
-    document.getElementById("quizScreen").classList.remove("hidden");
+    document.getElementById("quizScreen").classList.add("active");
+    // document.getElementById("quizScreen").style.display = "flex";
 })
 
-playAgain.addEventListener("click", ()=> {
-    document.getElementById("quizScreen").classList.remove("hidden");
+playAgain.addEventListener("click", () => {
+    document.getElementById("quizScreen").classList.add("active");
     document.getElementById("endScreen").classList.add("hidden");
-    
+
     getQuestions();
     countDown(60);
     score = 0;
