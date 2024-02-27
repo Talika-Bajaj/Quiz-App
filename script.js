3ae46d40fb1d6293350e654ebe74564e14f38c1d
Talika Bajaj <bajajtalika2001@gmail.com>
2024-02-27 14:16:06 +0530

    Add comments

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
+//setting score to 0
 let score = 0;
-let startQuiz = document.getElementById("startQuiz");
-let playAgain = document.getElementById("playAgain");
+let startQuiz = document.getElementById("start-quiz");
+let playAgain = document.getElementById("play-again");
 
-
+//async function to fetch api
 async function getQuestions() {
     let question = await fetch("https://opentdb.com/api.php?amount=1");
     let data = await question.json();
     console.log(data);
-    console.log(data.response_code);
+    // console.log(data.response_code);
     try {
         showQuestion(data.results[0]);
-    } catch(error) {
+    } catch (error) {
         document.getElementById("question").innerHTML = "Error fetching question.Please try again";
     }
 
@@ -18,55 +19,51 @@ async function getQuestions() {
 
 getQuestions();
 
-
+//function to display questions from api
 function showQuestion(data) {
 
-        console.log(data);
-        console.log(data.correct_answer);
+    // console.log(data);
+    console.log(data.correct_answer);
 
-        question.innerHTML = data.question;
-        const { correct_answer, incorrect_answers } = data;
-        const answer = [correct_answer, ...incorrect_answers];
-        console.log(answer);
+    question.innerHTML = data.question;
+    const { correct_answer, incorrect_answers } = data;
+    const answer = [correct_answer, ...incorrect_answers];
+    // console.log(answer);
 
-        answer.sort(() => Math.random() - 0.5);
-        console.log(answer);
+    answer.sort(() => Math.random() - 0.5);
+    // console.log(answer);
 
-        // let answers = document.querySelectorAll(".answer");
-        // for (let i = 0; i< answer.length; i++) {
-        //     answers[i].innerHTML = answer[i];
-        // }
-        let answersList = document.querySelector(".answersList")
-        answersList.innerHTML = `
+    let answersList = document.querySelector(".answers-list")
+    answersList.innerHTML = `
     ${answer.map((ans) =>
-            `<button class="btn">${ans}</button>`
-        ).join(" ")} `;
-        // let answersList = document.querySelector(".answersList")
-        answersList.querySelectorAll("button").forEach(element => {
-            if (element.innerHTML !== correct_answer) {
-                element.addEventListener("click", ()=> {
-                    element.style.backgroundColor = "#a42626";
-                    setTimeout(()=> {
-                        getQuestions();
-                    }, 500)
-                });
-            } else {
-                element.addEventListener("click", ()=> {
-                    score += 10;
-                    console.log("it is correct");
-                    element.style.backgroundColor = "mediumseagreen";
-                    setTimeout(()=> {
-                        getQuestions();
-                        updateScore();
+        `<button class="btn">${ans}</button>`
+    ).join(" ")} `;
 
-                    }, 500)
-                });
-            }
+    answersList.querySelectorAll("button").forEach(element => {
+        if (element.innerHTML !== correct_answer) {
+            element.addEventListener("click", () => {
+                element.style.backgroundColor = "#a42626";
+                setTimeout(() => {
+                    getQuestions();
+                }, 500)
+            });
+        } else {
+            element.addEventListener("click", () => {
+                score += 10;
+                console.log("it is correct");
+                element.style.backgroundColor = "mediumseagreen";
+                setTimeout(() => {
+                    getQuestions();
+                    updateScore();
+                }, 500)
+            });
+        }
 
-        });
-    
+    });
+
 }
 
+//function to run countdown
 function countDown(seconds) {
     let counter = seconds;
 
@@ -81,7 +78,7 @@ function countDown(seconds) {
     }, 1000)
 }
 
-
+//function for correct answer
 function correctAnswer() {
     score += 10;
     console.log("it is correct");
@@ -89,16 +86,17 @@ function correctAnswer() {
     updateScore();
 }
 
-
+//function to update the score
 function updateScore() {
     document.getElementById("score").innerHTML = score;
 }
 
+// function to display remarks
 function endQuiz() {
-    document.getElementById("quizScreen").classList.remove("active");
-    document.getElementById("endScreen").classList.add("active");
-    let endMsg = document.getElementById("endMessage");
-    let finalScore = document.getElementById("finalScore");
+    document.getElementById("quiz-screen").classList.remove("active");
+    document.getElementById("end-screen").classList.add("active");
+    let endMsg = document.getElementById("end-message");
+    let finalScore = document.getElementById("final-score");
 
     if (score <= 20) {
         endMsg.innerHTML = `<span style = "color: black">Remarks: </span> You played poor`;
@@ -111,18 +109,19 @@ function endQuiz() {
     finalScore.innerHTML = `<span style = "font-weight:600">Final score: </span> ${score}`;
 }
 
-
+//event listener to start the quiz
 startQuiz.addEventListener("click", () => {
     countDown(60);
     document.getElementById("start-rules").style.display = "none";
-    document.getElementById("startScreen").style.display = "none";
-    document.getElementById("quizScreen").classList.add("active");
+    document.getElementById("start-screen").style.display = "none";
+    document.getElementById("quiz-screen").classList.add("active");
     // document.getElementById("quizScreen").style.display = "flex";
 })
 
+//event handler to play the game again
 playAgain.addEventListener("click", () => {
-    document.getElementById("quizScreen").classList.add("active");
-    document.getElementById("endScreen").classList.remove("active");
+    document.getElementById("quiz-screen").classList.add("active");
+    document.getElementById("end-screen").classList.remove("active");
 
     getQuestions();
     countDown(60);
